481d5e727cb6916041152d9a892b694a9bafc376
Mayank Sharma <msharma@iitk.ac.in>
2019-02-09 07:46:05 +0530

    Fixed bug when multiple ping commands are spawned. Handled cases for stopping Ping

--- A2/VM3/server/app.ts
+++ A2/VM3/server/app.ts
@@ -13,50 +13,66 @@ const app = express();
 const server = require('http').Server(app);
 const io = require( 'socket.io' )(server);
 
-app.use(cors());
-app.use(httpLogger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
+const env_PORT = Number.parseInt(process.env.SERVER_PORT || "8000");
 
-let sendObj = null;
-let VM_Config = {
-	vm1_IP: null,
-	vm2_IP: null,
-	timeInterval: null
-};
+class WebServer {
+	private sendObj = {};
+	private VM_Config = {
+		vm1_IP: null,
+		vm2_IP: null,
+		timeInterval: 1
+	};
 
-process.nextTick(() => {
-	const vm1_name = 'vm1_c';
-	const vm2_name = 'vm2_c';
-	const docker_inspect = exec(`docker inspect ${vm1_name} ${vm2_name}`);
-	docker_inspect.then((data) => {
-		let config = JSON.parse(data.stdout);
-		config = config.map((VM) => {
-			return {
-				Name: VM.Name,
-				IP: VM.NetworkSettings.Networks.bridge.IPAddress
-			};
-		});
-		VM_Config.vm1_IP = config[0].IP;
-		VM_Config.vm2_IP = config[1].IP;
-		sendObj = {
+	constructor(config){
+		this.sendObj = {
 			config: config
 		};
-	})
-		.catch((err) => {
-			throw err;
-		});
-});
-const env_PORT = Number.parseInt(process.env.SERVER_PORT || "8000");
-app.get('/getIP', IPHandler);
-app.use('/*', ErrorHandler);
+		console.log(this.sendObj);
+		this.VM_Config.vm1_IP = config[0].IP;
+		this.VM_Config.vm2_IP = config[1].IP;
+		app.use(cors());
+		app.use(httpLogger('dev'));
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended : false }));
+		app.get('/getIP', this.getIPHandler);
+		app.use('/*', this.ErrorHandler);
+		io.on('connection', socket => {
+			/*socket.emit('pingICMP', {
+			 *  msg: 'Started Pinging the Virtual Machines'
+			 *});*/
+			let pingProcess_vm1;
+			let pingProcess_vm2;
+			socket.on('getPing', (data) => {
+				console.log(data);
+				this.VM_Config.timeInterval = data.interval;
+				// Ping requires minimum 0.2 seconds spacing between two attempts
+				pingProcess_vm1 = this.pinger(socket, this.VM_Config.vm1_IP);
+				pingProcess_vm2 = this.pinger(socket, this.VM_Config.vm2_IP); 
+			});
 
-function pinger(socket, IP) {
-		if(VM_Config.timeInterval < 0.2) return;
+			socket.on('stopPing', () => {
+				console.log('stopPing emitted!');
+				if (typeof pingProcess_vm1 !== 'undefined')
+					pingProcess_vm1.kill();
+				if (typeof pingProcess_vm2 !== 'undefined')
+				pingProcess_vm2.kill();
+			})
+			socket.on('disconnect', () => {
+				if (typeof pingProcess_vm1 !== 'undefined')
+					pingProcess_vm1.kill();
+				if (typeof pingProcess_vm2 !== 'undefined')
+				pingProcess_vm2.kill();
+			});
+		});
+	}
+	private pinger = (socket, IP) => {
+		if(this.VM_Config.timeInterval < 0.2) return;
 		if(typeof IP !== 'string') return;
-		let pingProcess = cp.spawn('ping', ['-i', 
-			VM_Config.timeInterval.toString(),
-			IP
+		if(typeof this.VM_Config === 'undefined') return;
+		let pingProcess = cp.spawn('ping', 
+			['-i', 
+				this.VM_Config.timeInterval.toString(),
+				IP
 		]);
 		pingProcess.stdout.on('data', (data) => {
 			let str = data.toString();
@@ -69,40 +85,15 @@ function pinger(socket, IP) {
 		pingProcess.on('exit', (code, signal) => {
 			console.log(`child process exited with code ${code}, signal = ${signal}`);
 		});
-	return pingProcess;
-}
-io.on('connection', socket => {
-	socket.emit('pingICMP', {
-		msg: 'Started Pinging the Virtual Machines'
-	});
-	let pingProcess_vm1;
-	let pingProcess_vm2;
-	socket.on('getPing', function (data) {
-		console.log(data);
-		VM_Config.timeInterval = data.interval;
-		pingProcess_vm1 = pinger(socket, VM_Config.vm1_IP);
-		pingProcess_vm2 = pinger(socket, VM_Config.vm2_IP);
-		// Ping requires minimum 0.2 seconds spacing between two attempts
-	});
-
-	socket.on('disconnect', () => {
-		pingProcess_vm1.kill();
-		pingProcess_vm2.kill();
-	});
-});
-	function randomGenerator(req, res, next) {
-		const rand = Math.floor((Math.random() * 20) + 1);
-		res.status(200);
-		res.send({
-			randVal: rand
-		});
+		return pingProcess;
+	}
+	public getIPHandler = (req, res, next) => {
+		console.log(this.sendObj);
+		res.send(this.sendObj);
 		res.end();
 	}
-
-	function IPHandler(req, res, next) {
-		res.send(sendObj);
-	}
-	function ErrorHandler(err, req, res, next) {
+	private ErrorHandler = (err, req, res, next) => {
+		console.error(err);
 		// Assertion errors are wrong user inputs
 		if(err.name === 'SyntaxError' || 
 			err.code === 'ERR_ASSERTION' || 
@@ -122,9 +113,30 @@ io.on('connection', socket => {
 			res.end('500 - INTERNAL SERVER ERROR!');
 		}
 	}
-	server.listen(env_PORT, (err) => { 
-		if (err) {
+}
+
+process.nextTick(() => {
+	const vm1_name = 'vm1_c';
+	const vm2_name = 'vm2_c';
+	const docker_inspect = exec(`docker inspect ${vm1_name} ${vm2_name}`);
+	docker_inspect.then((data) => {
+		let config = JSON.parse(data.stdout);
+		config = config.map((VM) => {
+			return {
+				Name: VM.Name,
+				IP: VM.NetworkSettings.Networks.bridge.IPAddress
+			};
+		});
+		const expressApp = new WebServer(config);
+	})
+		.catch((err) => {
 			throw err;
-		}
-		console.log("Server listening on " + env_PORT);
-	});
+		});
+});
+
+server.listen(env_PORT, (err) => { 
+	if (err) {
+		throw err;
+	}
+	console.log("Server listening on " + env_PORT);
+});
